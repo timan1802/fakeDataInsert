a579b8b1e0a3253de5f910ceb0a8a68b10bdda1b
NDH <timan@naver.com>
2025-05-01 23:19:52 +0900

    테이블에서 우클릭일 경우만 팝업 띄우도록

--- src/main/kotlin/com/github/timan1802/fakedatainsert/GenerateFakeDataAction.kt
+++ src/main/kotlin/com/github/timan1802/fakedatainsert/GenerateFakeDataAction.kt
@@ -1,11 +1,28 @@
 package com.github.timan1802.fakedatainsert
 
-import com.intellij.openapi.actionSystem.AnAction
-import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.database.psi.DbTable
+import com.intellij.openapi.actionSystem.*
+import com.intellij.openapi.project.DumbAware
 
-class GenerateFakeDataAction : AnAction() {
+class GenerateFakeDataAction : AnAction(), DumbAware {
+
+    override fun getActionUpdateThread(): ActionUpdateThread {
+        return ActionUpdateThread.BGT
+    }
+
+    override fun update(e: AnActionEvent) {
+        val psiElement = e.getData(CommonDataKeys.PSI_ELEMENT)
+        val isTable = psiElement is DbTable
+
+        e.presentation.isVisible = isTable
+        e.presentation.isEnabled = isTable
+    }
+
     override fun actionPerformed(e: AnActionEvent) {
-        val dialog = DataFakerDialog()
-        dialog.show()
+        val psiElement = e.getData(CommonDataKeys.PSI_ELEMENT)
+        if (psiElement is DbTable) {
+            val dialog = DataFakerDialog()
+            dialog.show()
+        }
     }
 }

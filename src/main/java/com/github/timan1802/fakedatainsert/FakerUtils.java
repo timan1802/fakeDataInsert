be96327bbe3888157d750751e0e4526ca98a3529
NDH <timan@learnershi.com>
2025-05-07 10:30:49 +0900

    필요한 메소드만 반환.

--- src/main/java/com/github/timan1802/fakedatainsert/FakerUtils.java
+++ src/main/java/com/github/timan1802/fakedatainsert/FakerUtils.java
@@ -4,7 +4,10 @@ import net.datafaker.Faker;
 
 import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class FakerUtils {
 
@@ -28,26 +31,36 @@ public class FakerUtils {
     /**
      * 2. Provider 이름으로 해당 Provider의 모든 메서드 반환 (생성자 제외)
      */
-    public static List<String> getProviderMethodNames(Faker faker, String providerName) {
-        List<String> methodNames = new ArrayList<>();
-        try {
-            Method providerMethod = faker.getClass().getMethod(providerName);
-            Object provider = providerMethod.invoke(faker);
-
-            for (Method method : provider.getClass().getMethods()) {
-                if (!method.isSynthetic() &&
-                    !method.getName().equals("<init>") &&
-                    method.getDeclaringClass().getPackageName().startsWith("net.datafaker.providers.base")) {
-                    methodNames.add(method.getName());
-                }
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return methodNames;
+public static List<String> getProviderMethodNames(Faker faker, String providerName) {
+    try {
+        Method providerMethod = faker.getClass().getMethod(providerName);
+        Object provider = providerMethod.invoke(faker);
+        
+        // 메소드 필터링 및 이름 추출
+        return Arrays.stream(provider.getClass().getMethods())
+                     .filter(method ->
+                        // Object 클래스의 메소드 제외
+                        method.getDeclaringClass() != Object.class &&
+                        // void 반환 메소드 제외
+                        method.getReturnType() != void.class &&
+                        // 파라미터가 없는 메소드만 포함
+                        method.getParameterCount() == 0 &&
+                        // getter 메소드 제외
+                        !method.getName().startsWith("get") &&
+                        // setter 메소드 제외
+                        !method.getName().startsWith("set") &&
+                        // 기타 제외할 메소드들
+                        !Arrays.asList("equals", "hashCode", "toString", "stream").contains(method.getName())
+                    )
+                     .map(Method::getName)
+                     .sorted()
+                     .collect(Collectors.toList());
+                    
+    } catch (Exception e) {
+        e.printStackTrace();
+        return Collections.emptyList();
     }
+}
 
     /**
      * 3. Provider 이름과 메서드 이름을 받아 실행 결과값 반환
